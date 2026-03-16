1f20f3534b65680b60b637046552ce6dad8b10aa
mis7ake7411 <feng.school@gmail.com>
2026-03-16 06:15:11 +0000

    chore: initialize docflow lite skeleton and docker baseline

---
+++ src/main/java/com/docflow/DocflowLiteApplication.java
@@ -0,0 +1,12 @@
+package com.docflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocflowLiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocflowLiteApplication.class, args);
+    }
+}
